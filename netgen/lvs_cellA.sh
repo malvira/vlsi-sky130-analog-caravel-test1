beb4f4251e280ef83e1d5ef5bce4ba4bb2e72360
Mariano Alvira <mar@devl.org>
2021-05-19 22:30:59 -0600

    lvs runs now (needed the testbech on the schematic side to get the includes). They don't match.

--- netgen/lvs_cellA.sh
+++ netgen/lvs_cellA.sh
@@ -1,1 +1,1 @@
-netgen -batch lvs "../mag/cellA.spice cellA" "../xschem/cellA.spice cellA" /usr/share/pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl comp.out
+netgen -batch lvs "../mag/cellA.spice cellA" "../xschem/cellA_tb.spice cellA" /usr/share/pdk/sky130A/libs.tech/netgen/sky130A_setup.tcl comp.out
